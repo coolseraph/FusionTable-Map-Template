cbcd0fed00573a99ee50e715632d9ce4a42c58e7
coolseraph <coolseraph+github@gmail.com>
2013-02-28 18:30:31 +0000

    Update maps_lib.js

--- source/maps_lib.js
+++ source/maps_lib.js
@@ -34,8 +34,8 @@ var MapsLib = {
   recordName:         "result",       //for showing number of results
   recordNamePlural:   "results",
 
-  searchRadius:       805,            //in meters ~ 1/2 mile
-  defaultZoom:        11,             //zoom level when map is loaded (bigger is more zoomed in)
+  searchRadius:       10000,            //in meters ~ 1/2 mile
+  defaultZoom:        9,             //zoom level when map is loaded (bigger is more zoomed in)
   addrMarkerImage: 'http://derekeder.com/images/icons/blue-pushpin.png',
   currentPinpoint: null,
 
